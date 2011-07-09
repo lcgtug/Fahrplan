b04d633fcb28f1413ff108a4c55c16749cb3060d
FaKod <info@fakod.eu>
2011-07-09 13:07:34 +0200

    added example

--- src/main/scala/org/lcgtug/Main.scala
+++ src/main/scala/org/lcgtug/Main.scala
@@ -1,14 +1,32 @@
 package org.lcgtug
 
+import org.neo4j.scala.{EmbeddedGraphDatabaseServiceProvider, Neo4jWrapper}
+import org.neo4j.graphdb.{Direction, DynamicRelationshipType}
+
 /**
- * 
+ *
  * @author Christopher Schmidt
  * Date: 09.07.11
  * Time: 12:08
  */
 
-object Main extends App {
+object Main extends App with Neo4jWrapper with EmbeddedGraphDatabaseServiceProvider {
 
+  def neo4jStoreDir = "/tmp/temp-neo-test"
+
+  Runtime.getRuntime.addShutdownHook(new Thread() {
+    override def run() {
+      ds.gds.shutdown
+    }
+  })
+
+  withTx {
+    implicit neo =>
+    val start = createNode
+    val end = createNode
+    val relType = DynamicRelationshipType.withName("foo")
+    start --> relType --> end
+  }
 
   println("Hello LCGTUG Hackathon")
 }
